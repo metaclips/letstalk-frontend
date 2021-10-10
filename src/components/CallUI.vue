b493f77b66f321f08c83decbde49b5a414f5e321
Michael Uti <utimichael9@gmail.com>
2021-10-10 19:53:45 +0100

    Add video UI

--- src/components/CallUI.vue
+++ src/components/CallUI.vue
@@ -1,4 +1,137 @@
-<template></template>
+<template>
+  <v-container
+    fill-height
+    align="center"
+    justify="center"
+    style="min-width: 100%; min-height: 100%; background-color: black"
+    @click="showVideoOptions = !showVideoOptions"
+  >
+    <video
+      style="
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        position: absolute;
+        z-index: 0;
+        object-fit: cover;
+      "
+      id="videoID"
+      autoplay
+    ></video>
+
+    <v-fab-transition>
+      <v-row
+        style="
+          width: 100vw;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          position: relative;
+          z-index: 1;
+        "
+        justify="center"
+        v-if="showVideoOptions"
+      >
+        <v-col cols="12" class="mx-5 d-flex justify-end">
+          <v-menu
+            v-model="showConnectedUsers"
+            left
+            :close-on-content-click="false"
+            nudge-width="300"
+            offset-y
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                icon
+                dark
+                right
+                :x-large="!$vuetify.breakpoint.smAndDown"
+              >
+                <v-icon>mdi-account-circle</v-icon>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-row class="mx-10">
+                <v-col cols="12"> </v-col>
+                <v-col
+                  cols="12"
+                  v-for="(onlineUsers, index) in connectedUsers"
+                  :key="index"
+                >
+                  <v-badge
+                    inline
+                    dot
+                    :color="onlineUsers.isOnline ? 'green' : 'red'"
+                  ></v-badge>
+
+                  <span>
+                    <b class="mx-2 d-inline-block text--secondary">{{
+                      onlineUsers.name
+                    }}</b>
+                  </span>
+                </v-col>
+                <v-col cols="12"> </v-col>
+              </v-row>
+            </v-card>
+          </v-menu>
+        </v-col>
+
+        <v-col cols="12" class="my-10 d-flex align-end">
+          <v-row justify="center" v-if="showVideoOptions">
+            <v-btn
+              class="ml-2"
+              small
+              fab
+              dark
+              @click="desktopShare()"
+              :color="isDesktopShared ? 'blue' : ''"
+            >
+              <v-icon>mdi-desktop-mac</v-icon>
+            </v-btn>
+
+            <v-btn class="ml-2" fab small dark @click="changeVideoStatus()">
+              <v-icon>{{ videoIcon }}</v-icon>
+            </v-btn>
+
+            <v-btn
+              class="ml-2"
+              color="red"
+              small
+              fab
+              dark
+              @click="endCallSession()"
+            >
+              <v-icon>mdi-phone-hangup</v-icon>
+            </v-btn>
+
+            <v-btn class="ml-2" small fab dark @click="changeAudioStatus()">
+              <v-icon>{{ audioIcon }}</v-icon>
+            </v-btn>
+
+            <v-btn
+              :color="fullScreen ? 'blue' : ''"
+              class="ml-2"
+              small
+              fab
+              dark
+              @click="changeFullScreenStatus()"
+            >
+              <v-icon>mdi-fullscreen</v-icon>
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-fab-transition>
+  </v-container>
+</template>
 
 <script lang="ts">
 import Vue from "vue";
@@ -40,7 +173,7 @@ export default Vue.extend({
       if (!this.isDesktopShared) {
         this.startDesktopSharing();
       } else {
-        this.stopDesktopSharing(this.videoOn);
+        this.stopDesktopSharing();
       }
 
       this.isDesktopShared = !this.isDesktopShared;
@@ -70,7 +203,7 @@ export default Vue.extend({
       }
 
       this.audioOn = !this.audioOn;
-      this.changeAudio(this.audioOn);
+      this.changeAudio();
     },
 
     changeFullScreenStatus: function () {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,71 @@
 <template>
   <v-app>
     <v-row no-gutters>
-      <v-col :cols="$vuetify.breakpoint.smAndDown ? '2' : '1'">
-        <SideBar
-          :key="sideBarKey"
-          :highlightedSidebarOption="highlightedSidebarOption"
-          :unreadNotifications="unreadNotifications"
-          :unreadRoomMessages="unreadRoomMessages"
-          :activateNotificationDialog="activateNotificationDialog"
-          :activateAddRoomDialog="activateAddRoomDialog"
-          :deactivateAllDialogs="deactivateAllDialogs"
-          :showAvailableRooms="showAvailableRooms"
+      <template v-if="showCallUI">
+        <CallUI
+          :changeAudio="changeAudio"
+          :changeVideo="changeVideo"
+          :connectedUsers="classSessionConnectedUsers"
+          :endCallSession="endCallSession"
+          :isCallPublisher="isPublisher"
+          :startDesktopSharing="startDesktopSharing"
+          :stopDesktopSharing="stopDesktopSharing"
         />
-      </v-col>
+      </template>
 
-      <v-col v-if="showAddRoomDialog">
-        <CreateRoom ref="createRoom" :createRoom="createRoom" />
-      </v-col>
-      <v-col v-else-if="showNotificationDialog">
-        <Notification :joinRequests="joinRequests" :joinRoom="joinRoom" />
-      </v-col>
+      <template v-else>
+        <v-col :cols="$vuetify.breakpoint.smAndDown ? '2' : '1'">
+          <SideBar
+            :key="sideBarKey"
+            :highlightedSidebarOption="highlightedSidebarOption"
+            :unreadNotifications="unreadNotifications"
+            :unreadRoomMessages="unreadRoomMessages"
+            :activateNotificationDialog="activateNotificationDialog"
+            :activateAddRoomDialog="activateAddRoomDialog"
+            :deactivateAllDialogs="deactivateAllDialogs"
+            :showAvailableRooms="showAvailableRooms"
+          />
+        </v-col>
 
-      <v-col
-        v-else-if="showAvailableRoomsPage"
-        :cols="$vuetify.breakpoint.smAndDown ? '10' : '11'"
-      >
-        <v-row no-gutters>
-          <v-col cols="3">
-            <RoomsPage
-              :key="roomsPageKey"
-              :sendWSMessage="sendWSMessage"
-              :changeViewedRoomIndex="changeViewedRoomIndex"
-              :indexOfCurrentViewedRoom="indexOfCurrentViewedRoom"
-              :joinedRooms="joinedRooms"
-              :unreadRoomMessages="unreadRooms"
-            />
-          </v-col>
-          <v-col cols="9">
-            <ChatPage
-              :key="chatPageKey"
-              v-if="showChatPage"
-              :associatesMap="associatesNameMap"
-              :associates="associatesOnline"
-              :sendWSMessage="sendWSMessage"
-              :currentViewedRoom="currentViewedRoom"
-              :clearFetchedUsers="clearFetchedUsers"
-              :fetchedUsers="fetchedUsers"
-              :joinCallSession="joinCallSession"
-              :startCallSession="startCallSession"
-            />
-          </v-col>
-        </v-row>
-      </v-col>
+        <v-col v-if="showAddRoomDialog">
+          <CreateRoom ref="createRoom" :createRoom="createRoom" />
+        </v-col>
+        <v-col v-else-if="showNotificationDialog">
+          <Notification :joinRequests="joinRequests" :joinRoom="joinRoom" />
+        </v-col>
+
+        <v-col
+          v-else-if="showAvailableRoomsPage"
+          :cols="$vuetify.breakpoint.smAndDown ? '10' : '11'"
+        >
+          <v-row no-gutters>
+            <v-col cols="3">
+              <RoomsPage
+                :key="roomsPageKey"
+                :sendWSMessage="sendWSMessage"
+                :changeViewedRoomIndex="changeViewedRoomIndex"
+                :indexOfCurrentViewedRoom="indexOfCurrentViewedRoom"
+                :joinedRooms="joinedRooms"
+                :unreadRoomMessages="unreadRooms"
+              />
+            </v-col>
+            <v-col cols="9">
+              <ChatPage
+                :key="chatPageKey"
+                v-if="showChatPage"
+                :associatesMap="associatesNameMap"
+                :associates="associatesOnline"
+                :sendWSMessage="sendWSMessage"
+                :currentViewedRoom="currentViewedRoom"
+                :clearFetchedUsers="clearFetchedUsers"
+                :fetchedUsers="fetchedUsers"
+                :joinCallSession="joinCallSession"
+                :startCallSession="startCallSession"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </template>
     </v-row>
 
     <v-dialog persistent width="400px" v-model="showDialog">
@@ -94,12 +108,23 @@
 <script lang="ts">
 let socket: WebSocket;
 
+let videoTransceiver: any;
+let audioTransceiver: any;
+
+let videoTrack: MediaStreamTrack;
+let audioTrack: MediaStreamTrack;
+let stream: MediaStream;
+
+let peerConnection: RTCPeerConnection;
+
 // @ is an alias to /src
 import SideBar from "@/components/SideBar.vue";
 import CreateRoom from "@/components/CreateRoom.vue";
 import RoomsPage from "@/components/RoomsPage.vue";
 import ChatPage from "@/components/ChatPage.vue";
 import Notification from "@/components/Notification.vue";
+import CallUI from "@/components/CallUI.vue";
+
 import store from "@/store";
 
 import Vue from "vue";
@@ -110,6 +135,7 @@ import {
   JoinRequest,
   Message,
   FetchedUsers,
+  UsersOnline,
 } from "../Types";
 import { WSMessageType, SideBarOptions } from "../Constants";
 
@@ -122,6 +148,7 @@ export default Vue.extend({
     RoomsPage,
     ChatPage,
     Notification,
+    CallUI,
   },
 
   data: () => ({
@@ -129,6 +156,9 @@ export default Vue.extend({
     showAddRoomDialog: false,
     showAvailableRoomsPage: true,
     showChatPage: false,
+    showCallUI: false,
+
+    isPublisher: false,
 
     highlightedSidebarOption: "Rooms",
 
@@ -139,10 +169,13 @@ export default Vue.extend({
     sideBarKey: 0,
     roomsPageKey: 0,
     chatPageKey: 0,
+    callPageKey: 0,
 
     joinedRooms: [] as JoinedRoom[],
     fetchedUsers: [] as FetchedUsers[],
     joinRequests: [] as JoinRequest[],
+
+    classSessionConnectedUsers: {} as UsersOnline,
 
     currentViewedRoom: {} as RoomPageDetails,
 
@@ -158,11 +191,377 @@ export default Vue.extend({
 
   methods: {
     startCallSession: function () {
-      console.log("Hello");
+      this.showCallUI = true;
+      this.isPublisher = true;
+
+      this.classSessionConnectedUsers = {};
+      this.classSessionConnectedUsers[store.state.userID] = {
+        name: store.state.userID,
+        isOnline: true,
+      };
+
+      peerConnection = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: ["stun:stun.l.google.com:19302"],
+          },
+        ],
+      });
+
+      peerConnection.oniceconnectionstatechange = () => {
+        console.log("ICE connection state", peerConnection.iceConnectionState);
+
+        if (peerConnection.iceConnectionState === "failed") {
+          // Show error page so that users can choose to disconnect call.
+          this.dialogError = "Peer connection failed.";
+          this.showDialog = true;
+        }
+      };
+
+      peerConnection.onicecandidate = (event) => {
+        console.log("New candidate added", event);
+
+        if (
+          event.candidate === null &&
+          peerConnection.localDescription &&
+          peerConnection.localDescription.sdp &&
+          peerConnection.iceConnectionState === "new"
+        ) {
+          const message = {
+            msgType: WSMessageType.StartClassSession,
+            sdp: peerConnection.localDescription.sdp,
+            roomID: this.currentViewedRoom.roomID,
+          };
+
+          socket.send(JSON.stringify(message));
+        }
+      };
+
+      peerConnection.onnegotiationneeded = (event) => {
+        console.log("Negotiation needed", event);
+      };
+
+      const MediaConstraints = {
+        video: { width: 640, height: 480 },
+        audio: true,
+      };
+
+      this.getUserMedia(MediaConstraints, (e: MediaStream) => {
+        videoTrack = e.getVideoTracks()[0];
+        audioTrack = e.getAudioTracks()[0];
+
+        audioTransceiver = peerConnection.addTransceiver(audioTrack, {
+          direction: "sendrecv",
+        });
+
+        videoTransceiver = peerConnection.addTransceiver(videoTrack, {
+          direction: "sendrecv",
+        });
+
+        const el = document.getElementById("videoID") as HTMLVideoElement;
+
+        if (!el) {
+          this.dialogError = "Error getting video element.";
+          this.showDialog = true;
+          console.log("Could not get video ID element");
+          return;
+        }
+
+        const mediaStreamTrack = [videoTrack];
+        stream = new MediaStream(mediaStreamTrack);
+
+        el.srcObject = stream;
+        el.autoplay = true;
+
+        peerConnection
+          .createOffer()
+          .then((sdp) => {
+            peerConnection.setLocalDescription(sdp);
+          })
+          .catch((log) =>
+            console.log("Error creating peer connection offer, error:", log)
+          );
+      });
+
+      peerConnection.ontrack = ({ transceiver, streams: [event] }) => {
+        event.onaddtrack = (event) => {
+          console.log("On add track called for start video session.");
+          stream.addTrack(event.track);
+          console.log(event.track.id);
+        };
+
+        event.onremovetrack = (event) => {
+          console.log("On remove track called for start video session.");
+          stream.removeTrack(event.track);
+          console.log(event.track.id);
+        };
+
+        transceiver.receiver.track.onmute = () => {
+          console.log("Track muted for start session.");
+        };
+
+        transceiver.receiver.track.onended = () => {
+          console.log("Track ended for start session");
+
+          const id = event.id;
+
+          this.classSessionConnectedUsers[id] = {
+            name: id,
+            isOnline: false,
+          };
+
+          this.callPageKey++;
+        };
+
+        transceiver.receiver.track.onunmute = () => {
+          console.log("Track started for start session.");
+          console.log("track", transceiver, "\n event", event);
+
+          const id = event.id;
+          this.classSessionConnectedUsers[id] = {
+            name: id,
+            isOnline: true,
+          };
+
+          stream.addTrack(transceiver.receiver.track);
+        };
+      };
     },
 
-    joinCallSession: function () {
-      console.log("Hello");
+    joinCallSession: function (sessionData: Message) {
+      this.showCallUI = true;
+      this.isPublisher = false;
+      this.classSessionConnectedUsers = {};
+
+      peerConnection = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: ["stun:stun.l.google.com:19302"],
+          },
+        ],
+      });
+
+      peerConnection.oniceconnectionstatechange = () => {
+        console.log(peerConnection.iceConnectionState);
+
+        if (peerConnection.iceConnectionState === "failed") {
+          this.dialogError = "Connection state failed";
+          this.showDialog = true;
+        }
+      };
+
+      peerConnection.onnegotiationneeded = (event) => {
+        console.log("Negotiation needed", event);
+      };
+
+      peerConnection.onicecandidate = (event) => {
+        console.log("A new user added", event);
+
+        if (
+          event.candidate == null &&
+          peerConnection.localDescription &&
+          peerConnection.localDescription.sdp &&
+          peerConnection.iceConnectionState === "new"
+        ) {
+          console.log("Calling join class session");
+
+          const message = {
+            msgType: WSMessageType.JoinClassSession,
+            sdp: peerConnection.localDescription.sdp,
+            roomID: this.currentViewedRoom.roomID,
+            sessionID: sessionData.message,
+          };
+          socket.send(JSON.stringify(message));
+        }
+      };
+
+      const MediaConstraints = {
+        audio: true,
+      };
+
+      this.getUserMedia(MediaConstraints, (e: MediaStream) => {
+        console.log("Adding audio track");
+        audioTrack = e.getAudioTracks()[0];
+        audioTrack.enabled = true;
+        audioTransceiver = peerConnection.addTransceiver(audioTrack, {
+          direction: "sendrecv",
+        });
+
+        const el = document.getElementById("videoID") as HTMLVideoElement;
+
+        if (!el) {
+          this.dialogError = "Error getting video element.";
+          this.showDialog = true;
+
+          console.log("Could not get video ID element");
+          return;
+        }
+
+        stream = new MediaStream();
+
+        el.srcObject = stream;
+        el.autoplay = true;
+
+        console.log("Sending to server to create offer.");
+        peerConnection
+          .createOffer()
+          .then((sdp) => {
+            peerConnection.setLocalDescription(sdp);
+          })
+          .catch((log) => {
+            console.log("Error creating offer on join session, error:", log);
+            this.dialogError = "Error creating peer offer";
+            this.showDialog = true;
+          });
+      });
+
+      peerConnection.ontrack = ({ transceiver, streams: [event] }) => {
+        event.onaddtrack = (event) => {
+          console.log("On add track called for join video session");
+          stream.addTrack(event.track);
+          console.log("track ID", event.track.id);
+        };
+
+        event.onremovetrack = (track) => {
+          console.log("On remove track called for join video session.");
+          stream.removeTrack(track.track);
+          console.log(track.track.id);
+
+          const id = track.track.id;
+          delete this.classSessionConnectedUsers[id];
+
+          this.callPageKey++;
+        };
+
+        transceiver.receiver.track.onmute = () =>
+          console.log("Track muted for join session");
+
+        transceiver.receiver.track.onended = () => {
+          console.log("Track ended for Join session");
+        };
+
+        transceiver.receiver.track.onunmute = () => {
+          if (transceiver.receiver.track.kind === "audio") {
+            const id = transceiver.receiver.track.id;
+            this.classSessionConnectedUsers[id] = {
+              name: event.id,
+              isOnline: true,
+            };
+          }
+
+          console.log("Track unmuted");
+
+          console.log("Track started for Join session");
+
+          console.log("track", transceiver, "\n event", event);
+          stream.addTrack(transceiver.receiver.track);
+        };
+      };
+    },
+
+    endCallSession: function () {
+      this.showCallUI = false;
+
+      if (audioTrack) {
+        audioTrack.enabled = false;
+        audioTrack.stop();
+        audioTransceiver.stop();
+      }
+
+      if (videoTrack) {
+        videoTrack.enabled = false;
+        videoTrack.stop();
+        videoTransceiver.stop();
+      }
+
+      if (peerConnection) peerConnection.close();
+
+      this.isPublisher = false;
+      console.log("sending websocket peer close message");
+    },
+
+    changeVideo: function () {
+      videoTrack.enabled = !videoTrack.enabled;
+
+      videoTrack.enabled
+        ? videoTransceiver.sender.replaceTrack(videoTrack)
+        : videoTransceiver.sender.replaceTrack(null);
+    },
+
+    changeAudio: function () {
+      audioTrack.enabled = !audioTrack.enabled;
+      audioTrack.enabled
+        ? audioTransceiver.sender.replaceTrack(audioTrack)
+        : audioTransceiver.sender.replaceTrack(null);
+    },
+
+    getUserMedia: function (
+      mediaConstraints: any,
+      onMedia: (e: MediaStream) => void
+    ) {
+      navigator.mediaDevices
+        .getUserMedia(mediaConstraints)
+        .then(function (e) {
+          onMedia(e);
+        })
+        .catch((error) => {
+          console.log("Error getting media device, error:", error);
+          this.dialogError = "Error starting media devices";
+          this.showDialog = true;
+        });
+    },
+
+    startDesktopSharing: function () {
+      // 480p video constraints.
+      const mediaConstraints = { video: { width: 640, height: 480 } };
+
+      navigator.mediaDevices
+        .getDisplayMedia(mediaConstraints)
+        .then((event) => {
+          // Disable webcam and remove video from stream.
+          videoTrack.enabled = false;
+          stream.removeTrack(videoTrack);
+
+          const tracks = event.getVideoTracks();
+          this.addTracks(tracks);
+
+          if (tracks.length > 0) {
+            videoTransceiver.sender.replaceTrack(tracks[0]);
+          } else {
+            console.log("Could not find any track for desktop sharing");
+
+            this.dialogError = "Could not get track for desktop sharing.";
+            this.showDialog = true;
+          }
+        })
+        .catch((error) => {
+          console.log("Error creating desktop sharing", error);
+          this.dialogError = "Error starting desktop sharing.";
+          this.showDialog = true;
+        });
+    },
+
+    stopDesktopSharing: function (isVideoOn: boolean) {
+      const tracks = stream.getVideoTracks();
+      this.removeTracks(tracks);
+
+      videoTrack.enabled = isVideoOn;
+      stream.addTrack(videoTrack);
+
+      videoTransceiver.sender.replaceTrack(videoTrack);
+    },
+
+    removeTracks: function (tracks: MediaStreamTrack[]) {
+      for (let i = 0; i < tracks.length; i++) {
+        tracks[i].enabled = false;
+        stream.removeTrack(tracks[i]);
+      }
+    },
+
+    addTracks: function (tracks: MediaStreamTrack[]) {
+      for (let i = 0; i < tracks.length; i++) {
+        stream.addTrack(tracks[i]);
+      }
     },
 
     activateNotificationDialog: function () {
